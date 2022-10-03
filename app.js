80af7556207b39eb7adec2814bd2cdd461f8362e
ALBERTO HERNANDEZ <albertohgj@hotmail.com>
2022-10-02 19:22:09 -0500

    cambio de puerto

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const express = require('express');
 const PORT = 3000;
 
 var app = express();
-app.use(express.json());
+//app.use(express.json());
 
 //app.use('/');
 
-app.get('/', (req,res)=> res.send("hola mundo"))
+app.get('/', (req,res)=> res.send("hola mundo"));
 
-app.listen(PORT, ()=>{
-    console.log("Servidor escuchando en el puerto",PORT);
+app.listen(process.env.PORT||PORT, ()=>{
+    console.log("Servidor escuchando en el puerto",process.env.PORT||PORT);
 });
 
